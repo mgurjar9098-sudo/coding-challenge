74ae26133c0ea52b078f720c512f18dfeba5eb31
mgurjar9098-sudo <m.gurjar9098@gmail.com>
2026-07-25 11:19:24 +0530

    comilted

---
+++ oop.py
@@ -0,0 +1,297 @@
+# class student:
+
+#     def __init__(self,name,last):
+#         self.name=name
+#         self.last=last
+
+
+
+# s=student("ram",'gurjar')
+# print(s.name)
+# print(s.last)
+
+
+# class student:
+
+#     def __init__(self,name,last):
+#         self.name=name
+#         self.last=last
+
+#     def show(self):
+#         print(f"name is {self.name} and last name is {self.last}")
+
+
+
+# s=student("ram",'gurjar')
+# s.show()
+
+
+
+# class voter:
+
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+
+#     def show(self):
+#         print(f"your name is {self.name} and age is {self.age}")
+
+#     def is_above(self):
+#         if self.age>=18:
+#             return True
+#         else:
+#             return False
+
+# v=voter("ram",18)
+# print(v.is_above())
+# v.show()
+
+
+# class varible
+# class Circle:
+#     pi=3.14
+#     def __init__(self,redius):
+#         self.redius=redius
+
+#     def cir_refrece(self):
+#         return 2*self.pi*self.redius
+
+# c=Circle(3)
+# print(c.cir_refrece())
+
+
+
+# return data in dictinary formate
+
+# class person:
+
+#     def __init__(self,name,age,city):
+#         self.name=name
+#         self.age=age
+#         self.city=city
+
+
+# p=person("mohan",18,'mds')
+# print(p.__dict__)
+
+
+# class student:
+#     name='government'
+
+#     @classmethod
+#     def show_name(cls):
+#         print("this is class method")
+#         print(cls.name)
+
+# student.show_name()
+
+
+
+# class student:
+#     count=0
+#     def __init__(self,name):
+#         student.count+=1
+#         self.name=name
+
+#     @classmethod
+#     def count_obj(cls):
+#         return cls.count
+
+# s=student('ram')
+# s2=student('mohan')
+# s3=student('richa')
+# print(student.count_obj())
+
+
+# Class method as constctor
+
+# class student:
+
+#     def __init__(self,name,age,roll):
+#         self.name=name
+#         self.age=age
+#         self.roll=roll
+
+#     def detail(self):
+#         print(f"name is {self.name} and age is {self.age} and {self.roll}")
+
+#     @classmethod
+#     def from_string(cls,string):
+#         name,age,roll=string.split()
+#         return cls(name,age,roll)
+# s=student('ram',32,101)
+# s1=student.from_string('mohan 34 102')
+# s.detail()
+# s1.detail()
+
+
+
+# static method
+# class Person:
+
+#     @staticmethod
+#     def show():
+#         print("this is static method")
+
+# # obj=Person()
+# # obj.show()
+
+# Person.show()
+
+
+
+# getter and setter method in oops
+# class person:
+
+#     def __init__(self,name,age):
+#         self.__name=name
+#         self.__age=age
+
+#     def get_name(self):
+#         return self.__name
+
+#     def set_name(self,name):
+#         if isinstance(name,str) and len(name)>0:
+#             self.__name=name
+#         else:
+#             print("invalid name please enter valid name")
+
+#     def get_age(self):
+#         return self.__age
+
+#     def set_age(self,age):
+
+#         if isinstance(age,int) and age>0:
+#             self.__age=age
+#         else:
+#             print("invalid age")
+
+# p=person("ram",18)
+# print(p.get_name())
+# print(p.get_age())
+
+# p.set_age(22)
+# print(p.get_age())
+
+# p.set_name('mohan')
+# print(p.get_name())
+
+
+# single level inheritence
+
+
+
+# class phone:
+
+#     def __init__(self,brand_name,model,price):
+#         self.brand_name=brand_name
+#         self.model=model
+#         self.price=price
+
+#     def detail(self):
+#         print(f"mobile name is {self.brand_name} and brand {self.brand_name} and price {self.price}")
+
+# class smartPhone(phone):
+#     def __init__(self,brand_name,model,price,cam):
+#         super().__init__(brand_name,model,price)
+#         self.cam=cam
+
+#     def detail(self):
+#         print(f"mobile name is {self.brand_name} and brand {self.model} and price {self.price} and cam is {self.cam}")
+
+
+# s=smartPhone('vivo','y14',12000,'15px')
+# s.detail()
+
+
+
+# second method
+
+# class phone:
+
+#     def __init__(self,brand_name,model,price):
+#         self.brand_name=brand_name
+#         self.model=model
+#         self.price=price
+
+#     def detail(self):
+#         print(f"mobile name is {self.brand_name} and brand {self.brand_name} and price {self.price}")
+
+# class smartPhone(phone):
+#     def __init__(self,brand_name,model,price,cam):
+#         phone.__init__(self,brand_name,model,price)
+#         self.cam=cam
+
+#     def detail(self):
+#         print(f"mobile name is {self.brand_name} and brand {self.model} and price {self.price} and cam is {self.cam}")
+
+
+# s=smartPhone('oppo','z5',15000,'50px')
+# s.detail()
+
+
+
+# multilevel inheritence:
+
+# class GrandFather:
+#     def  display_grand(self):
+#         print("This is grand father class")
+
+
+# class Father(GrandFather):
+
+#     def display_father(self):
+#         print("this is father class")
+
+
+# class Son(Father):
+
+#     def display_son(self):
+#         print("this is son class")
+
+    
+# c=Son()
+# c.display_son()
+# c.display_father()
+# c.display_grand()
+
+
+
+# multiple inheritence 
+
+# class Father:
+
+#     def father(self,fname):
+#         self.fname=fname
+      
+
+# class Mother(Father):
+#         def Mother(self,mname):
+#             self.mname=mname
+            
+
+# class Son(Mother):
+#         def son(self,sname):
+#             self.sname=sname
+
+#         def detail(self):
+#             print(f'student name is {self.sname}')
+#             print(f'father name is {self.fname}')
+#             print(f'mother name is {self.mname}')
+
+# s=Son()
+# s.father("ashok")
+# s.Mother('radharaman bai')
+
+# s.son('rahul')
+# s.detail()
+
+
+# abstarcation
+
+
+
+
+
+
+
