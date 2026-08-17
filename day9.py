f55031c6c29ea17a8a44798215c1fcda33a02456
mgurjar9098-sudo <m.gurjar9098@gmail.com>
2026-08-17 17:16:30 +0530

    Update day9.py

---
+++ day9.py
@@ -0,0 +1,24 @@
+#Write a Python program to check if a number is positive, negative or zero. 
+
+# num=int(input("enter the number"))
+
+# if num>0:
+#     print('positive')
+# elif num<0:
+#     print('negetive')
+# else:
+#     print('zero')
+
+
+#  Write a Python program to get the Factorial number of given number. 
+
+# num=int(input("enter the number"))
+
+# fact=1
+# for i in range(1,num+1):
+#     fact=fact*i
+
+# print("Factorial is ",fact)
+
+
+
