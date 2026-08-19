3e6698539bf8168c54e5180b33d49d4e547211f9
mgurjar9098-sudo <m.gurjar9098@gmail.com>
2026-08-19 12:14:14 +0530

    Update day10.py

--- day10.py
+++ day10.py
@@ -18,3 +18,55 @@
 
 # print(f"{str} is total length {l}")
 
+
+
+
+#  Write a Python program to count the number of characters (character 
+#  frequency) in a string 
+# 
+
+# st="python is easy" 
+
+# freq={}
+# for w in st:
+#     if w !=" ":
+#         freq[w]=freq.get(w,0)+1
+
+# print(freq)
+
+
+#  What are negative indexes and why are they used?  
+
+# txt='python'
+# print(txt[-1])
+# print(txt[-1::-1])
+
+
+#  Write a Python program to count occurrences of a substring in a string.  
+
+# String = "hello hello world"
+# Substring = "hello"
+
+# c=String.count(Substring)
+# print(c)
+
+# other way
+
+# String = "hello hello world"
+# s=String.split()
+# Substring = "hello"
+
+# count=0
+# for i in s:
+#     if i==Substring:
+#         count+=1
+
+# print("total count is ",count)
+
+
+
+
+
+
+
+
