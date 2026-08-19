3aae1e92867372988fc182a252cbdac6cd338586
mgurjar9098-sudo <m.gurjar9098@gmail.com>
2026-08-19 11:55:22 +0530

    code

---
+++ day10.py
@@ -0,0 +1,20 @@
+# Write a python program to sum of the first n positive integers.
+
+# n=int(input("enter the nth number "))
+# sum=0
+# i=0
+# while(i<=n):
+#     sum=sum+i
+#     i=i+1
+
+# print(f'sum is {sum}')
+
+#  Write a Python program to calculate the length of a string.  
+
+# str='python to python program '
+# l=0
+# for i in str:
+#         l+=1
+
+# print(f"{str} is total length {l}")
+
--- day9.py
+++ day9.py
@@ -22,3 +22,101 @@
 
 
 
+# Write a Python program to get the Fibonacci series of given range.  
+
+# num=int(input("enter the number"))
+# a=-1
+# b=1
+# i=1
+# while(i<=num):
+#     c=a+b
+#     print(c,end=" ")
+#     a=b
+#     b=c
+
+#     i=i+1
+
+
+# Write python program that swap two number with temp variable and 
+# without temp variable. 
+
+
+# with temp varible 
+# num1=int(input("enter the first number "))
+# num2=int(input("enter the second number "))
+
+# print(f"num1 is ",num1)
+# print(f"num1 is ",num2)
+
+# temp=num1
+# num1=num2
+# num2=temp
+
+# # print('with temp varible')
+# # print(f"num1 is ",num1)
+# # print(f"num1 is ",num2)
+
+# num1,num2=num2,num1
+# print('without temp varible')
+# print(f"num1 is ",num1)
+# print(f"num1 is ",num2)
+
+
+# num=int(input('enter the number'))
+
+# if num%2==0:
+#     print('number is even number')
+
+# else:
+#     print("number is odd number")
+
+
+# Write a Python program to test whether a passed letter is a vowel or 
+# not. 
+
+# char=input("enter the char")
+
+# if char=='i' or char=='a' or char=='e' or char=='o' or char=='u' or char=='I' or char=='A' or char=='E' or char=='O' or char=='U' :
+#     print('vowel')
+# else:
+#     print('not vowel')
+
+#  Write a Python program to sum of three given integers. However, if 
+# two values are equal sum will be zero.  
+
+# num1=int(input("enter the first number "))
+# num2=int(input("enter the second number "))
+# num3=int(input("enter the third number "))
+
+# if num1==num2 or num1==num3  or num2==num3:
+#     print('0')
+# else:
+#     print(num1+num2+num3)
+
+
+
+# Write a Python program that will return true if the two given integer 
+# values are equal or their sum or difference is 5. 
+
+
+num1=int(input('enter the first number'))
+num2=int(input('enter the second number'))
+
+if num1==num2 or num1+num2==5 or num1-num2==5:
+    print(True)
+
+else:
+    print(False)
+
+
+
+
+
+
+
+
+
+
+
+
+
