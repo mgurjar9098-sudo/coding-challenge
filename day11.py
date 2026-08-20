fa3ee6ae862a2a646d5ae9c62057d69d32adacc4
mgurjar9098-sudo <m.gurjar9098@gmail.com>
2026-08-20 12:20:57 +0530

    code

---
+++ day11.py
@@ -0,0 +1,22 @@
+# Write a Python program to find the first appearance of the substring 
+# 'not' and 'poor' from a given string, if 'not' follows the 'poor', replace the 
+# whole 'not'...'poor' substring  with 'good'. Return the resulting string.
+
+
+# string="The movie is not very poor"
+
+# not_pos=string.find('not')
+# poor_pos=string.find('poor')
+
+# if not_pos != -1 and poor_pos != -1 and not_pos < poor_pos:
+#     string = string[:not_pos] + "good" + string[poor_pos + 4:]
+#     print(string)
+
+
+
+# Write a Python function that takes a list of words and returns the length 
+# of the longest one.  
+
+words = ["apple", "banana", "cat", "elephant"]
+
+longest=0
